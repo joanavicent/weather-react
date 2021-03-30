0865ed4955f201b70b861e5d22a995398656c06b
joanavicent <75983479+joanavicent@users.noreply.github.com>
2021-03-30 12:38:16 +0100

    Added Github url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <div className="link">
+        <a href="https://github.com/joanavicent/weather-react">Open-source code </a>
+    by Joana Vicente
     </div>
+    </div>
+    
   );
 }
 
