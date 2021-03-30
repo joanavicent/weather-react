ded8aada26bf002a52aa56df891a3c5848e33c00
joanavicent <75983479+joanavicent@users.noreply.github.com>
2021-03-30 14:15:10 +0100

    Added Forecast
    Forecast structure/CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
 import Weather from "./Weather";
+import Forecast from "./Forecast";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
       <Weather />
+      <Forecast />
       <footer>
         This project was coded by Joana Vicente and is {" "}
       <a href="https://github.com/joanavicent/weather-react" target="_blank">Open-sourced on GitHub </a>
---
+++ src/Forecast.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Forecast.css";
+export default function Forecast() {
+  return (
+    <div className="row" id="forecast">
+      <div className="col-2">
+        <div className="card">
+          <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" />
+          <div className="card-body">
+            <p className="card-temperature">15°C</p>
+            <hr />
+            <p className="card-day">18:00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-2">
+        <div className="card">
+          <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" />
+          <div className="card-body">
+            <p className="card-temperature">15°C</p>
+            <hr />
+            <p className="card-day">18:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-2">
+        <div className="card">
+          <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" />
+          <div className="card-body">
+            <p className="card-temperature">15°C</p>
+            <hr />
+            <p className="card-day">18:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-2">
+        <div className="card">
+          <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" />
+          <div className="card-body">
+            <p className="card-temperature">15°C</p>
+            <hr />
+            <p className="card-day">18:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-2">
+        <div className="card">
+          <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" />
+          <div className="card-body">
+            <p className="card-temperature">15°C</p>
+            <hr />
+            <p className="card-day">18:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-2">
+        <div className="card">
+          <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" />
+          <div className="card-body">
+            <p className="card-temperature">15°C</p>
+            <hr />
+            <p className="card-day">18:00</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
