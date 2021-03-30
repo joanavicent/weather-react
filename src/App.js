cea081fb2cc83346e50e8786640e4e01fb26974b
joanavicent <75983479+joanavicent@users.noreply.github.com>
2021-03-30 12:45:56 +0100

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <div className="link">
         <a href="https://github.com/joanavicent/weather-react">Open-source code </a>
     by Joana Vicente
+    </div>
     </div>
     </div>
     
