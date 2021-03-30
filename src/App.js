75cef2b44a368729271de7c9cc631f98788023de
joanavicent <75983479+joanavicent@users.noreply.github.com>
2021-03-30 13:05:22 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App
-      <div className="link">
-        <a href="https://github.com/joanavicent/weather-react">Open-source code </a>
-    by Joana Vicente
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Joana Vicente and is
+      <a href="https://github.com/joanavicent/weather-react" target="_blank">Open-sourced on GitHub </a>
+      </footer>
     </div>
     </div>
-    </div>
-    
   );
 }
 
