516e1d5cae6453dc8f2ce9a6308851217887ca5d
joanavicent <75983479+joanavicent@users.noreply.github.com>
2021-03-31 16:41:11 +0100

    Update Form

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Form.css";
 
-export default function Form() {
+
+export default function Form(props) {
+    const[city, setCity]=useState (props.defaultCity);
+    function handleSubmit(event){
+        event.preventDefault();
+    }
+
+    function handleCityChange(event){
+setCity(event.target.value);
+    }
   return (
     <div>
-      <form id="city-search-form">
-        <input type="search" placeholder="Choose a city..." id="city-input" />
-        <input type="submit" value="🔎" className="btn tn-success" />
+      <form onSubmit= {handleSubmit} id="city-search-form">
+        <input 
+        type="search" 
+        placeholder="Enter a city..." 
+        id="city-input" 
+        autoFocus="on"
+        onChange={handleCityChange}
+        />
+        <input 
+        type="submit" 
+        value="🔎" 
+        className="btn tn-success" 
+        />
         <button id="current-location-button">
           <i className="fas fa-map-marker-alt"></i>
         </button>
